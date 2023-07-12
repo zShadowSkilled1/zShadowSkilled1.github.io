9add7728e1fe0bceff652b1b241107781b96120e
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-07-12 18:23:02 +0200

    Create script.js

---
+++ username-checker/script.js
@@ -0,0 +1,19 @@
+function checkUsername() {
+  var username = document.getElementById("username").value;
+
+  // Send an HTTP GET request to the server-side script
+  fetch("https://activecriticalsyndrome.shadowbot1.repl.co/?username=" + encodeURIComponent(username))
+    .then(response => response.text())
+    .then(data => {
+      if (data === "exists") {
+        document.getElementById("result").textContent = "This username is in the database.";
+      } else if (data === "added") {
+        document.getElementById("result").textContent = "Username added to the database.";
+      } else {
+        document.getElementById("result").textContent = "Error occurred.";
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
