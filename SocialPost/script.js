62184bea84e119250641f16bd05be747d0dea7ec
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-09-18 19:25:00 +0200

    Update script.js

--- SocialPost/script.js
+++ SocialPost/script.js
@@ -1,22 +1,15 @@
-function generateAndLogin() {
+function login() {
     var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-    // Generate a random password
-    var randomPassword = generateRandomPassword();
-
-    // Store the generated password in the hidden input field
-    document.getElementById("generatedPassword").value = randomPassword;
-
-    // Make the API request with the generated password
-    var url = "https://socialpost.shadowbot1.repl.co/?q=get&password=" + encodeURIComponent(randomPassword);
-
+    var url = "https://socialpost.shadowbot1.repl.co/?q=get&password=YOUR_PASSWORD"; // Replace with your actual password
     fetch(url)
         .then(response => response.text())
         .then(data => {
             var accounts = data.split('\n');
             for (var i = 0; i < accounts.length; i++) {
                 var [accountName, accountPassword] = accounts[i].split(':');
-                if (accountName === username && accountPassword === randomPassword) {
+                if (accountName === username && accountPassword === password) {
                     alert("Login successful!");
                     return;
                 }
@@ -24,17 +17,4 @@ function generateAndLogin() {
             alert("Invalid username or password.");
         })
         .catch(error => console.error("Error:", error));
-}
-
-function generateRandomPassword() {
-    // Generate a random password, e.g., using Math.random() or a library
-    // For simplicity, here's a basic example:
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var length = 8; // Adjust the length of the password as needed
-    var randomPassword = "";
-    for (var i = 0; i < length; i++) {
-        var randomIndex = Math.floor(Math.random() * characters.length);
-        randomPassword += characters.charAt(randomIndex);
-    }
-    return randomPassword;
 }
