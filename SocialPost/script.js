61bc730c6f82edc88fe3ad31a28dfe50107f01b3
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-09-18 19:17:13 +0200

    Update script.js

--- SocialPost/script.js
+++ SocialPost/script.js
@@ -2,19 +2,32 @@ function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    var url = "https://socialpost.shadowbot1.repl.co/?q=get&password=YOUR_PASSWORD"; // Replace with your actual password
-    fetch(url)
+    // Read the API endpoint and password from endpoint.txt
+    fetch('endpoint.txt')
         .then(response => response.text())
         .then(data => {
-            var accounts = data.split('\n');
-            for (var i = 0; i < accounts.length; i++) {
-                var [accountName, accountPassword] = accounts[i].split(':');
-                if (accountName === username && accountPassword === password) {
-                    alert("Login successful!");
-                    return;
-                }
+            var [endpoint, apiPassword] = data.split('&password=');
+
+            if (password === apiPassword) {
+                // Make an API request to check the account
+                var url = endpoint;
+                fetch(url)
+                    .then(response => response.text())
+                    .then(data => {
+                        var accounts = data.split('\n');
+                        for (var i = 0; i < accounts.length; i++) {
+                            var [accountName, accountPassword] = accounts[i].split(':');
+                            if (accountName === username && accountPassword === password) {
+                                alert("Login successful!");
+                                return;
+                            }
+                        }
+                        alert("Invalid username or password.");
+                    })
+                    .catch(error => console.error("Error:", error));
+            } else {
+                alert("Invalid API password.");
             }
-            alert("Invalid username or password.");
         })
         .catch(error => console.error("Error:", error));
 }
