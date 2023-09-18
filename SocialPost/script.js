cdbef99be41f4d8609e9247449e7fbc11dae1e8f
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-09-18 19:12:28 +0200

    Create script.js

---
+++ SocialPost/script.js
@@ -0,0 +1,20 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var url = "https://socialpost.shadowbot1.repl.co/?q=get&password=YOUR_PASSWORD"; // Replace with your actual password
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            var accounts = data.split('\n');
+            for (var i = 0; i < accounts.length; i++) {
+                var [accountName, accountPassword] = accounts[i].split(':');
+                if (accountName === username && accountPassword === password) {
+                    alert("Login successful!");
+                    return;
+                }
+            }
+            alert("Invalid username or password.");
+        })
+        .catch(error => console.error("Error:", error));
+}
