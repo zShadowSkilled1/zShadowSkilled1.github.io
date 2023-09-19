b4e018f1ce169d7bd449d4ced53aea707d10f71f
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-09-19 17:34:48 +0200

    Add files via upload

---
+++ zShadowSkilled/script.js
@@ -0,0 +1,38 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var url = "https://socialpost.shadowbot1.repl.co/?q=get&password=YOUR_PASSWORD"; // Replace with your actual password
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            var accounts = data.split('\n');
+            for (var i = 0; i < accounts.length; i++) {
+                var [accountName, accountPassword] = accounts[i].split(':');
+                if (accountName === username && accountPassword === password) {
+                    alert("Login successful!");
+                    window.location.href = "Main/Home/welcome.html"; // Redirect after successful login
+                    return;
+                }
+            }
+            alert("Invalid username or password.");
+        })
+        .catch(error => console.error("Error:", error));
+}
+
+function register() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var url = "https://socialpost.shadowbot1.repl.co/?q=create&account_name=" + username + "&account_password=" + password + "&password=YOUR_PASSWORD"; // Replace with your actual password
+
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            alert(data); // Display response message
+            if (data.includes("created successfully")) {
+                window.location.href = "Main/Home/welcome.html"; // Redirect after successful registration
+            }
+        })
+        .catch(error => console.error("Error:", error));
+}
