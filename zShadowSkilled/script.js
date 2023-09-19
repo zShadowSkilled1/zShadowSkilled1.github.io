4078ec72e547027aba0355c1037c04f93879b868
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-09-19 19:15:14 +0200

    Update script.js

--- zShadowSkilled/script.js
+++ zShadowSkilled/script.js
@@ -1,4 +1,4 @@
-function login() {
+function register() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
@@ -8,37 +8,31 @@ function login() {
         .then(response => response.text())
         .then(data => {
             var accounts = data.split('\n');
+            var accountExists = false;
+
             for (var i = 0; i < accounts.length; i++) {
                 var [accountName, accountPassword] = accounts[i].split(':');
-                if (accountName === username && accountPassword === password) {
-                    alert("Login successful!");
-                    setLoginCookies(username, password); // Save login information to cookies
-                    window.location.href = "Main/Home/welcome.html"; // Redirect after successful login
-                    return;
+                if (accountName === username) {
+                    accountExists = true;
+                    break;
                 }
             }
-            alert("Invalid username or password.");
-        })
-        .catch(error => console.error("Error:", error));
-}
 
-function setLoginCookies(username, password) {
-    document.cookie = `username=${username}`;
-    document.cookie = `password=${password}`;
-}
+            if (accountExists) {
+                alert("An account with the same name exists.");
+            } else {
+                // Continue with the registration process
+                var registerUrl = `https://socialpost.shadowbot1.repl.co/?q=create&account_name=${username}&account_password=${password}&password=YOUR_PASSWORD`; // Replace with your actual password
 
-function register() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    var url = "https://socialpost.shadowbot1.repl.co/?q=create&account_name=" + username + "&account_password=" + password + "&password=YOUR_PASSWORD"; // Replace with your actual password
-
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            alert(data); // Display response message
-            if (data.includes("created successfully")) {
-                window.location.href = "Main/Home/welcome.html"; // Redirect after successful registration
+                fetch(registerUrl)
+                    .then(response => response.text())
+                    .then(responseData => {
+                        alert(responseData); // Display response message
+                        if (responseData.includes("created successfully")) {
+                            window.location.href = "Main/Home/welcome.html"; // Redirect after successful registration
+                        }
+                    })
+                    .catch(error => console.error("Error:", error));
             }
         })
         .catch(error => console.error("Error:", error));
