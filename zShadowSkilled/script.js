983ff5e56b4c857d5828c3f3183ab46f2626163a
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-09-19 19:23:23 +0200

    Update script.js

--- zShadowSkilled/script.js
+++ zShadowSkilled/script.js
@@ -1,3 +1,32 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var url = "https://socialpost.shadowbot1.repl.co/?q=get&password=YOUR_PASSWORD"; // Replace with your actual password
+
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            var accounts = data.split('\n');
+            for (var i = 0; i < accounts.length; i++) {
+                var [accountName, accountPassword] = accounts[i].split(':');
+                if (accountName === username && accountPassword === password) {
+                    alert("Login successful!");
+                    setLoginCookies(username, password); // Save login information to cookies
+                    window.location.href = "Main/Home/welcome.html"; // Redirect after successful login
+                    return;
+                }
+            }
+            alert("Invalid username or password.");
+        })
+        .catch(error => console.error("Error:", error));
+}
+
+function setLoginCookies(username, password) {
+    document.cookie = `username=${username}`;
+    document.cookie = `password=${password}`;
+}
+
 function register() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -37,3 +66,4 @@ function register() {
         })
         .catch(error => console.error("Error:", error));
 }
+
