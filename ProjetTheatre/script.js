d900286d7cb4bb34b6e148953db45bad56109176
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-11-13 19:48:16 +0100

    Update script.js

--- ProjetTheatre/script.js
+++ ProjetTheatre/script.js
@@ -1,1 +1,13 @@
 
+// script.js
+function BonneReponse() {
+    document.getElementById("status").textContent = "Bonne Réponse";
+}
+
+function MauvaiseReponse() {
+    document.getElementById("status").textContent = "Mauvaise Réponse";
+    var button = document.createElement("button");
+    button.textContent = "Réponse Correcte";
+    button.onclick = BonneReponse;
+    document.body.appendChild(button);
+}
